2523327f96b361097e80b35593430250bea9a156
DominantCard903 <79110874+DominantCard903@users.noreply.github.com>
2025-06-26 15:35:49 -0700

    Formulario para agregar usuario

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'photo',
+            'role',
+            'created_at',
+            'updated_at'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'created_at': {'read_only': True},
+            'updated_at': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        # Aquí puedes agregar lógica para hashear la contraseña si lo deseas
+        return User.objects.create(**validated_data)
