5f11dbaaa8f40e03135b469556da2342ccc12365
Jonatan <jonatan16.05@hotmail.com>
2025-06-29 00:54:43 -0700

    CRUD de Herramientas con validaciones

--- tools/views.py
+++ tools/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Tool
+from .serializers import ToolSerializer
+
+class ToolViewSet(viewsets.ModelViewSet):
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
