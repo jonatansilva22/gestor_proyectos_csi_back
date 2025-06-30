494111b99d61a6dda9047f019b40506495e0457f
DominantCard903 <79110874+DominantCard903@users.noreply.github.com>
2025-06-30 10:45:35 -0700

    bcrypt agregado

---
+++ users/Crear_Usuario/serializers.py
@@ -0,0 +1,34 @@
+import bcrypt
+from rest_framework import serializers
+from users.models import User
+from users.Crear_Usuario.validations import validate_username, validate_email
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(validators=[validate_username])
+    email = serializers.EmailField(validators=[validate_email])
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'photo',
+            'role',
+            'created_at',
+            'updated_at'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'created_at': {'read_only': True},
+            'updated_at': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        raw_password = validated_data.pop('password')
+        hashed_password = bcrypt.hashpw(raw_password.encode('utf-8'), bcrypt.gensalt())
+        validated_data['password'] = hashed_password.decode('utf-8')
+        return User.objects.create(**validated_data)
